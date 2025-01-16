aed496a2ee30c366136322f8acbfec9c8b65f6ae
hungle8795 <quanghung8795@gmail.com>
2025-01-16 09:43:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Server/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
